b445072c22ff466a8fb63086073e890a3da73d38
Youngwok Kim <youngwok.kim83@gmail.com>
2023-05-31 10:34:26 -0400

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'Custom Embroidered Clothing & Our Custom Design'}
+        subtitle={'Quality Apparel'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
