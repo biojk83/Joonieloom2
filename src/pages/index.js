09042fc52549a9743ce8f6917099e87f6609b13e
James Kim <youngwok.kim83@gmail.com>
2023-06-04 12:21:02 -0400

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import ProductCardGrid from '../components/ProductCardGrid';
 import Quote from '../components/Quote';
 import Title from '../components/Title';
 import {ProductBrowser} from '@ecwid/gatsby-plugin-ecwid';
-import { Script } from "gatsby"
+import { Script } from 'gatsby';
 
 import { generateMockBlogData, generateMockProductData } from '../helpers/mock';
 
@@ -62,8 +62,8 @@ const IndexPage = () => {
       <div className={styles.messageContainer}>
       <div id="my-categories-86415007"></div>
         <div>
-        <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?86415007&data_platform=code&data_date=2023-06-04" charset="utf-8"></script>
-        <script type="text/javascript"> xCategoriesV2("id=my-categories-86415007"); </script>
+        <Script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?86415007&data_platform=code&data_date=2023-06-04" charset="utf-8"></Script>
+        <Script type="text/javascript"> xCategoriesV2("id=my-categories-86415007"); </Script>
         </div>
       </div>
 
