02a0fe306235510effadff040855f31a89414ab4
James Kim <youngwok.kim83@gmail.com>
2023-06-04 19:01:42 -0400

    a

--- src/components/ProductCollectionGrid/ProductCollectionGrid.js
+++ src/components/ProductCollectionGrid/ProductCollectionGrid.js
@@ -8,27 +8,27 @@ const ProductCollectionGrid = (props) => {
     <div className={styles.root}>
       <ProductCollection
         image={'/collections/collection1.png'}
-        title={'Men'}
-        text={'SHOP NOW'}
-        link={'/shop'}
+        title={'Hat'}
+        text={'Check Items'}
+        link={'/hat'}
       />
       <ProductCollection
         image={'/collections/collection2.png'}
         title={'Women'}
         text={'SHOP NOW'}
-        link={'/shop'}
+        link={'/hat'}
       />
       <ProductCollection
         image={'/collections/collection3.png'}
         title={'Accessories'}
         text={'SHOP NOW'}
-        link={'/shop'}
+        link={'/hat'}
       />
       <ProductCollection
         image={'/collections/collection4.png'}
         title={'Simple Cotton'}
         text={'SHOP NOW'}
-        link={'/shop'}
+        link={'/hat'}
       />
     </div>
   );
