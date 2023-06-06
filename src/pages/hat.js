0b20682b6d32856b68138489662e3e579e96b26d
James Kim <youngwok.kim83@gmail.com>
2023-06-06 08:23:31 -0400

    a

--- src/pages/hat.js
+++ src/pages/hat.js
@@ -26,7 +26,7 @@ const HatPage = (props) => {
         {/* Hero Container */}
         <Hero
           maxWidth={'900px'}
-          image={'/about.png'}
+          image={'/about.jpg'}
           title={`Hat \n Custom Embroidery for your logo`}
         />
 
