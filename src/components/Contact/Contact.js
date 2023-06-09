1b3e46530ff43ef9b0f253b5c53cd8505b502782
James Kim <youngwok.kim83@gmail.com>
2023-06-08 21:24:28 -0400

    a

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -30,28 +30,27 @@ const Contact = (props) => {
       <div className={styles.section}>
         <h4>Send Us A Message</h4>
         <p>
-          Our Customer Service team are here for all enquiries Monday to Friday,
-          9am - 5pm AEDT (Australian Eastern Daylight Savings Time).
+          Please send us a message if you have any questions - Monday to Friday,
+          9am - 5pm EST.
         </p>
         <p>We look forward to hearing from you.</p>
       </div>
 
       <div className={styles.section}>
-        <h4>Phone</h4>
-        <p>+1 424 280 4971</p>
-        <p>Monday to Friday - 9am - 5pm AEDT</p>
+        <h4>Email</h4>
+        <p>info.joonieloom@gmail.com</p>
+        <p>Monday to Friday - 9am - 5pm EST</p>
       </div>
 
       <div className={styles.section}>
         <h4>Email</h4>
         <p>
-          You can email our Customer Service team at customerservice@example.com
-          or via the contact form below:
+          You can email us via the contact form below:
         </p>
       </div>
 
       <div className={styles.contactContainer}>
-        <form onSubmit={(e) => handleSubmit(e)}>
+        <form onSubmit={(e) => handleSubmit(e)} method="POST" data-netlify="true">
           <div className={styles.contactForm}>
             <FormInputField
               id={'name'}
